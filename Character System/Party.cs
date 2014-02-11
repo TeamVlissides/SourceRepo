7f26e1532a6ab7e889952c52628babc3eb011ca8
Regan Fencl <reganfencl@aol.com>
2014-02-11 13:06:19 -0800

    Initial Character System Commit
    Regan just started writing some code for the character system. It's a
    class library project, for now.

---
+++ Character System/Party.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_System
+{/* start Character_System namespace */
+
+    public class Party
+    {/* start Party class */
+
+
+
+    }/* end Party class */
+
+}/* end Character_System namespace */

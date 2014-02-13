d56a66c27a6bcaa4657621bba593cf1b3f593366
Regan Fencl <reganfencl@aol.com>
2014-02-13 11:37:21 -0800

    Testing and Game System
    Created Testing.txt just for testing GitHub and created the Game System
    class.

---
+++ Game System/Game System/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_System
+{/* start Game_System namespace */
+    
+    public class Game
+    {/* start Game class */
+
+
+    }/* end Game class */
+
+}/* end Game_System namespace */

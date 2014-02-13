d85b26baeaef0607450076550bb1bfa822de3dd5
Regan Fencl <reganfencl@aol.com>
2014-02-13 12:36:53 -0800

    Started Run Loop and Wrote createCharacter method
    We're working on the games run loop to see how input and output would
    need to work. Wrote the createCharacter method to create the party for
    the player.

--- Game System/Game System/Game.cs
+++ Game System/Game System/Game.cs
@@ -14,28 +14,69 @@ namespace Game_System
         private Party mGoodGuys;
         private BattleSystem mBattle;
 
+        private bool mDragonDead = false;
+        private bool mVictory = true;
+
+        private Input mInput;
+        private Output mOutput;
+
         public void run()
         {/* start run */
 
+            bool createCharacter = false;
+
+            while (!mDragonDead && mVictory)
+            {/* start loop */
+
+                if (!createCharacter)
+                {/* start if */
+
+                    createCharacters();
+                    createCharacter = true;
+
+                }/* end if */
+
+
+
+            }/* end loop */
 
         }/* end run */
 
         public void notifyBattleOutcome( bool victory )
         {/* start notifyBattleOutcome */
 
+            mVictory = victory;
 
         }/* end notifyBattleOutcome */
 
         private void createCharacters()
         {/* start createCharacters */
 
+            int choice, i;
+            String[] classes = new String[ 6 ] { "Warrior", "Theif", "Monk", "WhiteMage", "BlackMage", "RedMage" };
+            String name;
+            Character[] characters = new Character[ Party.MAXPARTY ];
+
+            for (i = 0; i < Party.MAXPARTY; i++)
+            {/* start loop */
+
+                mOutput.sendOutput("What class type do you want? You get 3.");
+                mOutput.sendOutput(classes.GetEnumerator);
+                choice = mInput.getInput(TypeEnum.Int);
+                name = mInput.getInput(TypeEnum.String);
+
+                character[ i ] = CharacterCreationFactory(choice, name);
+
+            }/* end loop */
+
+            mGoodGuys = new Party(characters);
 
         }/* end createCharacters */
 
         public int getInput( int max )
         {/* start getInput */
 
-            return int.MaxValue;
+            return mInput.getInput(TypeEnum.Int);
 
         }/* end getInput */
 
@@ -46,10 +87,10 @@ namespace Game_System
 
         }/* end giveBattleOutput */
 
-        public void giveDungeonOutput( Grid grid )
+        public void notifyDugeonUpdate()
         {/* start giveDungeonOutput */
 
-
+            mOutput.sendOutput( mDungeon.getTileIterator() );
 
         }/* end giveDungeonOutput */
 

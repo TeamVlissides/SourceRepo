4552dc94a26298e520056cccd74fb902fb21e200
TeamVlissides <rholliday@eagles.ewu.edu>
2014-02-13 11:40:59 -0800

    Made changes to the Dungeon.cs test file
    Added a TestingMethod() in the Dungeon class.

---
+++ Testing/Dungeon.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonSystem2
+{
+    internal class Dungeon
+    {
+	
+		// This file is being used for testing...
+
+        // Attributes
+        private Grid mGrid;
+        private String mName;
+        private Game mgame;
+        private Party mGoodGuyParty;
+		
+		public void TestingMehthod()
+		{
+			int test;
+		}
+
+        // Methods
+        public void MovePlayer()
+        {
+
+        }
+
+        public Grid GetGrid()
+        {
+            return null;
+        }
+
+        public String GetDungeonName()
+        {
+            return null;
+        }
+
+        public Game GetGame()
+        {
+            return null;
+        }
+		
+		public void SetDungeonName(String name)
+		{
+		
+		}
+
+        public Party GetGoodGuyParty()
+        {
+            
+        }
+
+}
+}

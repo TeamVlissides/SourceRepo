fb05d67f88bf55a044db7d1fddd46f62dcd244ba
TeamVlissides <rholliday@eagles.ewu.edu>
2014-02-28 09:14:48 -0800

    Code changes in the Dungeon system
    I had added changes to work with the GUI system.

--- DungeonSystem2/DungeonSystem2/Dungeon.cs
+++ DungeonSystem2/DungeonSystem2/Dungeon.cs
@@ -13,8 +13,15 @@ namespace DungeonSystem2
         // Attributes
         private Grid mGrid;
         private String mName;
+        private Tile[,] tiles;
         private Game mGame;
         private Party mGoodGuyParty;
+
+        // Constructors
+        public Dungeon()
+        {
+            mGrid = new Grid(5, 5);
+        }
 
         public Grid GetGrid()
         {
@@ -28,9 +35,7 @@ namespace DungeonSystem2
 		
 		public void SetDungeonName(String name)
 		{
-
             mName = name;
-
 		}
         /* direction is the direction the party wants to go */
         public void getDirection( DirectionEnum direction )
@@ -49,7 +54,7 @@ namespace DungeonSystem2
             if( direction == DirectionEnum.UP )
                 /*...*/
 
-            notifyDungeonUpdate();
+            mGame.notifyDungeonUpdate();
 
             if( checkIfDragon() )
                 mGame.StartBattle(EnemyType.DRAGON);
@@ -58,6 +63,54 @@ namespace DungeonSystem2
                     mGame.StartBattle( EnemyType.NULL );
 
         }/* end getDirection */
+
+        public bool checkIfDragon()
+        {
+            return false;
+        }
+
+        public bool RollBattleDice()
+        {
+            return false;
+        }
+
+        // Interaction with other Systems code here...
+
+        /*
+         * So the Dungeon System knows where the characters(heros, enemies) are located on the 
+         * board, it knows the location of Treasure chest and what special abilities it will
+         * give to the characters, there are also walls, free space, treasure, dragon, that
+         * can be in the tiles. The dungeon also determines when a battle occurs,
+         * using the roll of the dice, 
+         * 
+         * There will not be a enemy image becuase the determination if there is a battle
+         * is by the roll of the dice. 
+         * 
+         * Change the Gem icon to a treasure box icon.
+         * 
+         * Implement statisitcs on the GUI.
+         *  - How does the GUI get updates about the statistics for the characters?
+         * 
+         * 
+         */
+
+        // get character location
+
+        // 
+
+        // get enemy location
+
+        // whatIsCurrentLocation(), eg, a wall, freespace, treasure, dragon, ect.
+
+        // make the Character be an observer of the Dungeon, so that it knows
+        // when a battle occurs, and the character should activate the battle system
+        // if a battle occurs on a tile. 
+
+        // should the Character also observe when the Tile has Treasure and then determine what to
+        // do with it.
+
+        // 
+
 
     }
 

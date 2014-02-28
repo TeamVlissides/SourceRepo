b8376fb76b2cd27ec5bfcd651ec80a682611dee5
Regan Fencl <reganfencl@aol.com>
2014-02-28 15:48:57 -0800

    Finished Abilities Factory
    Created all the abilities that will be in the game, for now.

--- Character System/AbilitiesFactory.cs
+++ Character System/AbilitiesFactory.cs
@@ -97,6 +97,63 @@ namespace Character_System
             if (ability == AbilitesEnum.BLUDGEON)
                 setBludgeonAbility();
 
+            if (ability == AbilitesEnum.PALM)
+                setPalmAbility();
+
+            if (ability == AbilitesEnum.BACKHAND)
+                setBackHandAbility();
+
+            if (ability == AbilitesEnum.JUDOCHOP)
+                setJudoChopAbility();
+
+            if (ability == AbilitesEnum.PUNCH)
+                setPunchAbility();
+
+            if (ability == AbilitesEnum.KICK)
+                setKickAbility();
+
+            if (ability == AbilitesEnum.TRIP)
+                setTripAbility();
+
+            if (ability == AbilitesEnum.POUNCE)
+                setPounceAbility();
+
+            if (ability == AbilitesEnum.BACKSTAB)
+                setBackStabAbility();
+
+            if (ability == AbilitesEnum.FEINT)
+                setFeintAbility();
+
+            if (ability == AbilitesEnum.SLICETHROAT)
+                setSliceThroatAbility();
+
+            if (ability == AbilitesEnum.APPLYBANDAID)
+                setApplyBandAidAbility();
+
+            if (ability == AbilitesEnum.SOOTH)
+                setSoothAbility();
+
+            if (ability == AbilitesEnum.REGENERATE)
+                setRegenerateAbility();
+
+            if (ability == AbilitesEnum.LUKEWARM)
+                setLukeWarmAbility();
+
+            if (ability == AbilitesEnum.HOT)
+                setHotAbility();
+
+            if (ability == AbilitesEnum.BLAZE)
+                setBlazeAbility();
+
+            if (ability == AbilitesEnum.FLAIL)
+                setFlailAbility();
+
+            if (ability == AbilitesEnum.FLICK)
+                setFlickAbility();
+
+            if (ability == AbilitesEnum.SLICE)
+                setSliceAbility();
+
             return new Ability(mDescription, mName, mType, mTargetSpread, mAffectEnemy, mBaseDamage, mCost);
 
         }/* end getAbility */
@@ -130,7 +187,7 @@ namespace Character_System
         private void setPrayAbility()
         {/* start setPrayAbility */
 
-            mDescription = "Pray to the Gods, and they will heal your allies with divine power";
+            mDescription = "Pray to the Gods, and they will heal your allies with divine power.";
             mName = "Pray";
             mType = Ability.MAGIC;
             mTargetSpread = Ability.PARTYTARGET;
@@ -169,7 +226,7 @@ namespace Character_System
         private void setFlashLightAbility()
         {/* start setFlashLightAbility */
 
-            mDescription = "Hurt an enemies eyes with a very bright light. Man, that's bright!";
+            mDescription = "Hurt an enemy's eyes with a very bright light. Man, that's bright!";
             mName = "Flash Light";
             mType = Ability.MAGIC;
             mTargetSpread = Ability.SINGLETARGET;
@@ -182,7 +239,7 @@ namespace Character_System
         private void setSparkAbility()
         {/* start setSparkAbility */
 
-            mDescription = "Hurt an enemies eyes with a REALLY bright light. Man, that's REALLY bright!";
+            mDescription = "Hurt an enemy's eyes with a REALLY bright light. Man, that's REALLY bright!";
             mName = "Spark";
             mType = Ability.MAGIC;
             mTargetSpread = Ability.SINGLETARGET;
@@ -274,7 +331,7 @@ namespace Character_System
         {/* start setFreezeAbility */
 
             mDescription = "Freeze an enemy's heart and have it stop beating.";
-            mName = "Cold";
+            mName = "Freeze";
             mType = Ability.MAGIC;
             mTargetSpread = Ability.SINGLETARGET;
             mAffectEnemy = Ability.DAMAGING;
@@ -286,15 +343,327 @@ namespace Character_System
         private void setBlizzardAbility()
         {/* start setBlizzardAbility */
 
-            mDescription = "Freeze an enemy's heart and have it stop beating.";
-            mName = "Cold";
+            mDescription = "Freeze all your enemies where they stand!";
+            mName = "Blizzard";
+            mType = Ability.MAGIC;
+            mTargetSpread = Ability.PARTYTARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 200;
+            mCost = 100;
+
+        }/* end setBlizzardAbility */
+
+        private void setPokeAbility()
+        {/* start setPokeAbility */
+
+            mDescription = "Poke an enemy...uncomfortably...";
+            mName = "Poke";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 20;
+            mCost = 10;
+
+        }/* end setPokeAbility */
+
+        private void setSlapAbility()
+        {/* start setSlapAbility */
+
+            mDescription = "Slap an enemy across the face! OUCH!";
+            mName = "Slap";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 40;
+            mCost = 15;
+
+        }/* end setSlapAbility */
+
+        private void setSmackAbility()
+        {/* start setSmackAbility */
+
+            mDescription = "The Rock is gonna lay the smackdown on your candy a**!";
+            mName = "Smack";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 80;
+            mCost = 20;
+
+        }/* end setSmackAbility */
+
+        private void setStrikeAbility()
+        {/* start setStrikeAbility */
+
+            mDescription = "Strike at the heart of an enemy!";
+            mName = "Strike";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 160;
+            mCost = 25;
+
+        }/* end setStrikeAbility */
+
+        private void setBludgeonAbility()
+        {/* start setBludgeonAbility */
+
+            mDescription = "Bludgeon an enemy to death!";
+            mName = "Bludgeon";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 320;
+            mCost = 30;
+
+        }/* end setBludgeonAbility */
+
+        private void setPalmAbility()
+        {/* start setPalmAbility */
+
+            mDescription = "Make an enemy talk to the hand!";
+            mName = "Palm";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 15;
+            mCost = 7;
+
+        }/* end setPalmAbility */
+
+        private void setBackHandAbility()
+        {/* start setBackHandAbility */
+
+            mDescription = "Make an enemy know you're Rick James!";
+            mName = "Back Hand";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 25;
+            mCost = 14;
+
+        }/* end setBackHandAbility */
+
+        private void setJudoChopAbility()
+        {/* start setJudoChopAbility */
+
+            mDescription = "Judo Chop!";
+            mName = "Judo Chop";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 65;
+            mCost = 17;
+
+        }/* end setJudoChopAbility */
+
+        private void setPunchAbility()
+        {/* start setPunchAbility */
+
+            mDescription = "Punch an enemy in the gut!";
+            mName = "Punch";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 130;
+            mCost = 23;
+
+        }/* end setPunchAbility */
+
+        private void setKickAbility()
+        {/* start setKickAbility */
+
+            mDescription = "Kick the enemy's head off!";
+            mName = "Kick";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 260;
+            mCost = 27;
+
+        }/* end setKickAbility */
+
+        private void setTripAbility()
+        {/* start setTripAbility */
+
+            mDescription = "Make an enemy fall over and hurt itself.";
+            mName = "Trip";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 10;
+            mCost = 5;
+
+        }/* end setTripAbility */
+
+        private void setPounceAbility()
+        {/* start setPounceAbility */
+
+            mDescription = "You jump onto an enemy. That kind of hurts...";
+            mName = "Pounce";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 20;
+            mCost = 10;
+
+        }/* end setPounceAbility */
+
+        private void setBackStabAbility()
+        {/* start setBackStabAbility */
+
+            mDescription = "Stab an enemy in the back!";
+            mName = "Back Stab";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 55;
+            mCost = 12;
+
+        }/* end setBackStabAbility */
+
+        private void setFeintAbility()
+        {/* start setFeintAbility */
+
+            mDescription = "Draw an enemy into a trap, and then grab them with your death grip!";
+            mName = "Feint";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 100;
+            mCost = 17;
+
+        }/* end setFeintAbility */
+
+        private void setSliceThroatAbility()
+        {/* start setSliceThroatAbility */
+
+            mDescription = "Slaughter an enemy by slicing their throat!";
+            mName = "Slice Throat";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 175;
+            mCost = 20;
+
+        }/* end setSliceThroatAbility */
+
+        private void setApplyBandAidAbility()
+        {/* start setApplyBandAidAbility */
+
+            mDescription = "Apply a Band-Aid® to an ally's wounds to stop a minute amount of bleeding.";
+            mName = "Apply Band-Aid®";
             mType = Ability.MAGIC;
             mTargetSpread = Ability.SINGLETARGET;
             mAffectEnemy = Ability.DAMAGING;
-            mBaseDamage = 120;
-            mCost = 50;
+            mBaseDamage = 3;
+            mCost = 3;
 
-        }/* end setBlizzardAbility */
+        }/* end setApplyBandAidAbility */
+
+        private void setSoothAbility()
+        {/* start setSoothAbility */
+
+            mDescription = "Use nature magic to sooth an ally's wounds.";
+            mName = "Sooth";
+            mType = Ability.MAGIC;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 10;
+            mCost = 10;
+
+        }/* end setSoothAbility */
+
+        private void setRegenerateAbility()
+        {/* start setRegenerateAbility */
+
+            mDescription = "Use nature magic to regenerate an ally's body and heal their wounds.";
+            mName = "Regenerate";
+            mType = Ability.MAGIC;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 40;
+            mCost = 40;
+
+        }/* end setRegenerateAbility */
+
+        private void setLukeWarmAbility()
+        {/* start setLukeWarmAbility */
+
+            mDescription = "Make an enemy uncomfortably warm.";
+            mName = "Luke Warm";
+            mType = Ability.MAGIC;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 3;
+            mCost = 3;
+
+        }/* end setLukeWarmAbility */
+
+        private void setHotAbility()
+        {/* start setHotAbility */
+
+            mDescription = "Make an enemy sweat. Enemies are allergic to sweat.";
+            mName = "Hot";
+            mType = Ability.MAGIC;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 50;
+            mCost = 25;
+
+        }/* end setHotAbility */
+
+        private void setBlazeAbility()
+        {/* start setBlazeAbility */
+
+            mDescription = "BURN 'EM! BURN ALL YOUR ENEMIES TO THE GROUND!";
+            mName = "Blaze";
+            mType = Ability.MAGIC;
+            mTargetSpread = Ability.PARTYTARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 150;
+            mCost = 75;
+
+        }/* end setBlazeAbility */
+
+        private void setFlailAbility()
+        {/* start setFlailAbility */
+
+            mDescription = "Just one \"l\" away from fail.";
+            mName = "Flail";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 7;
+            mCost = 3;
+
+        }/* end setFlailAbility */
+
+        private void setFlickAbility()
+        {/* start setFlickAbility */
+
+            mDescription = "You flick the ear of the enemy REALLY hard.";
+            mName = "Flick";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 40;
+            mCost = 10;
+
+        }/* end setFlickAbility */
+
+        private void setSliceAbility()
+        {/* start setSliceAbility */
+
+            mDescription = "Slice an enemy up into pieces!";
+            mName = "Slice";
+            mType = Ability.PHYSICAL;
+            mTargetSpread = Ability.SINGLETARGET;
+            mAffectEnemy = Ability.DAMAGING;
+            mBaseDamage = 150;
+            mCost = 17;
+
+        }/* end setSliceAbility */
 
     }/* end AbilitiesFactory class */
 

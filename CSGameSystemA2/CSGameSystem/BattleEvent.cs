e84342af6d0be7a7e98498f5a48c58ec586455ff
stevensonj <driverx241@gmail.com>
2014-03-14 23:21:52 -0700

    Minor Battlespace Update
    BattleEvent should be properly calling the gainExperience method. Also
    changed BattleEvent slightly so that the string returned by it tells
    when someone has been killed.

--- CSGameSystemA2/CSGameSystem/BattleEvent.cs
+++ CSGameSystemA2/CSGameSystem/BattleEvent.cs
@@ -14,6 +14,7 @@ namespace CSGameSystem
         private BattleAction mAction;
         private String actionOutcome;
         private Character mActee;
+        private String kill_string = "";
 
         public BattleEvent(Character currentActor, BattleAction actorAction, Character actorTarget)
         {
@@ -21,11 +22,22 @@ namespace CSGameSystem
             mAction = actorAction;
             actionOutcome = mAction.ToString();
             mActee = actorTarget;
+
+            if (mActee.isDead && !mActee.isPlayer)
+            {
+                ((PlayerCharacter)mActor).gainExperience(((Enemy)mActee).Worth);
+                kill_string = ", " + mActee.Name + " was vanquished!";
+            }
+
+            if (mActee.isDead && mActee.isPlayer)
+            {
+                kill_string = ", " + mActee.Name + " has been slain!";
+            }
         }
 
         public String toString()
         {
-            return mActor.Name + actionOutcome + mActee.Name;
+            return mActor.Name + actionOutcome + mActee.Name + kill_string;
         }
         
     }
